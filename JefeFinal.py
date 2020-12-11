3621c4b27f27789370cd45559aef7b2e177f2298
Cesar-2 <capimiento@utp.edu.co>
2020-12-11 03:41:40 -0500

    Que gonorrea

--- JefeFinal.py
+++ JefeFinal.py
@@ -2,20 +2,29 @@ import pygame
 import Constantes
 import numpy as np
 
+
 class JefeFinal(pygame.sprite.Sprite):
     def __init__(self, posicion):
         pygame.sprite.Sprite.__init__(self)
         self.accion = 0
-        self.imagenesDerecha = [pygame.image.load("Enemigo_abeja/Abeja1_derecha.png"),pygame.image.load("Enemigo_abeja/Abeja2_derecha.png")]
-        self.imagenesIzquierda = [pygame.image.load("Enemigo_abeja/Abeja1_izquierda.png"),pygame.image.load("Enemigo_abeja/Abeja2_izquierda.png")]
+        self.imagenesDerecha = [pygame.image.load("Ozarnus_Der/Kirby.png"), pygame.image.load("Ozarnus_Der/Kirby1.png"), pygame.image.load("Ozarnus_Der/Kirby2.png"), pygame.image.load(
+            "Ozarnus_Der/Kirby3.png"), pygame.image.load("Ozarnus_Der/Kirby4.png"), pygame.image.load("Ozarnus_Der/Kirb5.png"), pygame.image.load("Ozarnus_Der/Kirby6.png"), pygame.image.load("Ozarnus_Der/Kirby7.png")]
+        self.imagenesIzquierda = [pygame.image.load("Ozarnus_Izq/Kirby.png"), pygame.image.load("Ozarnus_Izq/Kirby1.png"), pygame.image.load("Ozarnus_Izq/Kirby2.png"), pygame.image.load(
+            "Ozarnus_Izq/Kirby3.png"), pygame.image.load("Ozarnus_Izq/Kirby4.png"), pygame.image.load("Ozarnus_Izq/Kirb5.png"), pygame.image.load("Ozarnus_Izq/Kirby6.png"), pygame.image.load("Ozarnus_Izq/Kirby7.png")]
         self.image = self.imagenesIzquierda[self.accion]
         self.rect = self.image.get_rect()
         self.rect.x = posicion[0]
         self.rect.y = posicion[1]
+        self.max_health = 1000
+        self.health = 1000
         self.direction = 0
         self.animacion = 0
 
-    def update(self,mov,posicion):
+    def healthbar(self, window):
+        pygame.draw.rect(window, (255,0,0), (self.rect.x-20, self.rect.y + 100 + 10, 100, 10))
+        pygame.draw.rect(window, (0,255,0), (self.rect.x-20, self.rect.y + 100 + 10, 100 * (self.health/self.max_health), 10))
+    
+    def update(self, mov, posicion):
         self.rect.x -= mov[0]
         self.rect.y -= mov[1]
         if self.animacion == 1:
@@ -31,12 +40,12 @@ class JefeFinal(pygame.sprite.Sprite):
         if self.rect.x < posicion[0]:
             self.direction = 1
         if self.direction == 0:
-            self.image = self.imagenesIzquierda[self.accion]          
+            self.image = self.imagenesIzquierda[self.accion]
         if self.direction == 1:
             self.image = self.imagenesDerecha[self.accion]
         x = posicion[0]-self.rect.x
         y = posicion[1]-self.rect.y
-        movimientox = int(x/np.sqrt(x**2+y**2)*5)
-        movimientoy = int(y/np.sqrt(x**2+y**2)*5)
+        movimientox = int(x/np.sqrt(x**2+y**2)*10)
+        movimientoy = int(y/np.sqrt(x**2+y**2)*10)
         self.rect.x += movimientox
         self.rect.y += movimientoy
